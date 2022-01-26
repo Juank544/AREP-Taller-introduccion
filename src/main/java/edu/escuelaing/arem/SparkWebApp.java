d10755e4f276a5b87f11d2a371af2075cc2b53af
unknown <juan.gil@labinfo.is.escuelaing.edu.co>
2022-01-26 14:23:52 -0500

    Get port S.O.

--- src/main/java/edu/escuelaing/arem/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/SparkWebApp.java
@@ -1,6 +1,6 @@
 package edu.escuelaing.arem;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 /**
  * Hello world!
@@ -11,6 +11,14 @@ public class SparkWebApp
     public static void main( String[] args )
     {
         System.out.println( "Initiating server... Ready" );
+        port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
     }
 }
